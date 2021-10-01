0d1f60c1806306c5fdc28758e3a1f957c8b79cfc
badB00l <salvolopes@gmail.com>
2021-10-01 19:40:10 +0200

    portfolio module and profile component have been generated and hooked to the routing module

--- portfolio/src/app/app-routing.module.ts
+++ portfolio/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from './user-portfolio/profile/profile.component';
+import { UserPortfolioModule } from './user-portfolio/user-portfolio.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/profile', pathMatch: 'full'},
+  {path: 'profile', component: ProfileComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), UserPortfolioModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
